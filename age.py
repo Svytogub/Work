46c1b5fbd349b1cefb4954b0e92fce9b8d84515f
Svytogub <81273509+Svytogub@users.noreply.github.com>
2021-03-31 18:12:03 +0200

    Create age.py

---
+++ age.py
@@ -0,0 +1,16 @@
+
+a = [3, 1930, 28, "Norma Shearer", "The Divorcee"	]
+e = [4, 1931, 63, "Marie Dressler", "Min and Bill"]
+r = [5, 1932, 32, "Helen Hayes", "The Sin of Madelon Claudet"	]
+t = [6, 1933, 26, "Katharine Hepburn", "Morning Glory"]
+y = [7, 1934, 31, "Claudette Colbert", "It Happened One Night"]
+u = [8, 1935, 27, "Bette Davis", "Dangerous"]
+i = [9, 1936, 27, "Luise Rainer", "The Great Ziegfeld"]
+o = [10, 1937, 28, "Luise Rainer", "The Good Earth"]
+
+age = [a[2] + e[2] + r[2] + t[2] + y[2] + u[2] + i[2] + o[2]]
+print(age)
+
+print('Average age = ', 2 / [age])
+
+
